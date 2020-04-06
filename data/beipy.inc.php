4c59dec5651c5990dbde8567f6be5b5f3dde0864
cyx200902 <62412516+cyx200902@users.noreply.github.com>
2020-04-06 18:31:48 +0800

    Add files via upload

---
+++ data/beipy.inc.php
@@ -0,0 +1,85 @@
+<?php
+ $aik =  array (
+  'sitename' => '七星影院全网解析',
+  'pcdomain' => 'http://qxkkk.bid/vip',
+  'title' => '七星影院VIP视频全网解析',
+  'keywords' => '优酷VIP解析,爱奇艺VIP解析,腾讯VIP解析,乐视VIP解析,芒果VIP解析,全网视频解析,VIP在线解析,视频VIP在线解析,VIP视频在线解析',
+  'description' => '本站为广大网友提供优酷VIP解析，爱奇艺VIP解析，腾讯VIP解析，乐视VIP解析，芒果VIP解析等解析服务，让你省去购买视频VIP费用，欢迎大家收藏本站，并将它介绍给您的朋友！',
+  'weibourl' => '',
+  'weibo' => '',
+  'gonggao' => '若视频播放异常，尝试刷新或更换接口即可解决！',
+  'gonggaourl' => 'http://qxkkk.bid',
+  'maurl' => 'http://v.yinyuetai.com/video/3255959',
+  'jiekou1name' => '默认一',
+  'jiekou1' => 'http://jx.9kyy.com/vip/?url=',
+  'jiekou2name' => '推荐二',
+  'jiekou2' => 'http://api.zuilingxian.com/jiexi.php?url=',
+  'jiekou3name' => '线路三',
+  'jiekou3' => 'http://y.mt2t.com/lines?url=',
+  'jiekou4name' => '线路四',
+  'jiekou4' => 'http://api.greatchina56.com/?url=',
+  'jiekou5name' => '线路五',
+  'jiekou5' => 'http://api.visaok.net/?url=',
+  'jiekou6name' => '线路六',
+  'jiekou6' => 'http://17kyun.com/api.php?url=',
+  'jiekou7name' => '磁力一',
+  'jiekou7' => 'http://api.lunvip.xyz/?url=',
+  'jiekou8name' => '磁力二',
+  'jiekou8' => 'http://www.ibb6.com/playm3u8/?url=',
+  'jiekou9name' => '测试线',
+  'jiekou9' => 'http://jx.618g.com/?url=',
+  'jiekou10name' => '解析',
+  'jiekou10' => 'http://qxkkk.bid/jx/jx.php?url=',
+  'jiekou11name' => '手机',
+  'jiekou11' => 'http://api.xiaomil.com/a/index.php?url=',
+  'jiekou12name' => '有广告',
+  'jiekou12' => 'http://at520.cn/jx/?url=',
+  'jiekou13name' => '云解析',
+  'jiekou13' => 'http://api.nobij.top/jx/?url=',
+  'jiekou14name' => '思古',
+  'jiekou14' => 'http://api.bbbbbb.me/ipsign/player.php?v=',
+  'jiekou15name' => '无广告',
+  'jiekou15' => 'http://jx.fuliz.cn/?url=',
+  'jiekou16name' => '线路七',
+  'jiekou16' => 'http://www.cqhwdnwx.com/jx/?url=',
+  'jiekou17name' => '搞笑',
+  'jiekou17' => 'https://api.pangujiexi.com/player.php?url=',
+  'jiekou18name' => '穷二代',
+  'jiekou18' => 'http://jx.ejiafarm.com/x/jiexi.php?url=',
+  'jiekou19name' => '思古解析',
+  'jiekou19' => 'http://api.bbbbbb.me/jx/?url=',
+  'jiekou20name' => '浮夸',
+  'jiekou20' => 'http://api.ifuxitv.com/vip/?url=',
+  'topnav' => '<li>
+
+	<a  href="http://qxkkk.f3322.net:8081/4/" target="_blank" title="伦理，成人，资源">成人站2</a>
+
+</li>
+
+
+',
+  'ch1' => '<a href="http://qxkkk.bid/yy/" target="_blank" title="伦理，成人，资源"><img src="img/gou.gif"/></a>',
+  'guanggao' => '使用方法：进入各大视频网站，找到想要观看的VIP视频，然后复制链接（浏览器上的视频地址），粘贴到视频的输入框，并点击“解析播放”即可！',
+  'changyan' => '',
+  'homelink' => '<a href="http://qxkkk.bid/" target="_blank"style="color:#888;margin:0 10px;">七星影院</a>|
+<a href="http://qxkkk.bid/yy/" target="_blank"style="color:#888;margin:0 10px;">成人影院</a>|
+<a href="http://qxkkk.bid/music/" target="_blank"style="color:#888;margin:0 10px;">七星音乐平台</a>|
+<a href="http://qxkkk.f3322.net:8081/" target="_blank"style="color:#888;margin:0 10px;">备用主站</a>|
+<a href="http://qxkkk.f3322.net:8081/1/" target="_blank"style="color:#888;margin:0 10px;">七星影视</a>|
+<a href="http://qxkkk.f3322.net:8081/2/" target="_blank"style="color:#888;margin:0 10px;">七星影院</a>|
+<a href="http://qxkkk.f3322.net:8081/3/" target="_blank"style="color:#888;margin:0 10px;">备用影视</a>|
+<a href="http://qxkkk.f3322.net:8081/4/" target="_blank"style="color:#888;margin:0 10px;">成人影院</a>|
+<a href="http://qxkkk.f3322.net:8081/5/" target="_blank"style="color:#888;margin:0 10px;">备用影院</a>|
+<a href="http://qxkkk.f3322.net:8081/8/" target="_blank"style="color:#888;margin:0 10px;">备用解析</a>',
+  'icp' => '京公网安备 8808088号',
+  'foot' => '本站提供的最新电影和电视剧资源均系收集于各大视频网站,本站只提供web页面服务,并不提供影片资源存储,也不参与录制、上传。<br/>',
+  'tongji' => '<script type="text/javascript">var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");document.write(unescape("%3Cspan id=\'cnzz_stat_icon_1263734298\'%3E%3C/span%3E%3Cscript src=\'" + cnzz_protocol + "s19.cnzz.com/z_stat.php%3Fid%3D1263734298%26show%3Dpic\' type=\'text/javascript\'%3E%3C/script%3E"));</script>',
+  'admin_name' => 'admin',
+  'admin_pass' => '3ceb0e9fb16f8673c35f707e8657124a',
+  'admin_email' => '10000@qq.com',
+  'hometopnotice' => '',
+  'hometopright' => '',
+  'sort' => '1,2,3,4,5,6,7,8,9,10',
+  'joinhotkey' => '0',
+);
+?>
---
+++ qx/foot.php
@@ -0,0 +1,3 @@
+<div id="footer">Copyright &copy; <span style="color:#999999">qxkkk.bid</span>, Powered by
+			<br/><a href="http://qxkkk.bid" target="_blank" style="color:#999999">七星影院版权所有</a>
+</div> 
